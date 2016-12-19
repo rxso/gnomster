12a26b7c62582a32d1f44f31f541054ccea2490d
rxso <rajat.soni@icloud.com>
2016-12-19 17:01:46 -0600

    Add comment model to incorporate features

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,10 @@
 class Place < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
+  
   geocoded_by :address
   after_validation :geocode
-  
+
   validates :name, presence: true, length: { minimum: 3 }
   validates :address, presence: true, length: { minimum: 10 }
   validates :description, presence: true, length: { minimum: 50 }
