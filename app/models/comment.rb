cb10853c9fd86697b7a930f2414c36a93c0a5f95
rxso <rajat.soni@icloud.com>
2016-12-19 23:16:52 -0600

    Add CSS style changes to improve colors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
+
+  RATINGS = {
+     '1 Star' => '1_star',
+     '2 Star' => '2_stars',
+     '3 Star' => '3_stars',
+     '4 Star' => '4_stars',
+     '5 Star' => '5_stars'
+  }
+
+  def ux_interface_rating
+    RATINGS.invert[self.rating]
+  end
 end
