b1d5f334b4c7c7381baa1f56b824e8dd7ada6f4f
rxso <rajat.soni@icloud.com>
2016-12-23 11:09:50 -0600

    Add coding to inmprove modal

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,12 +1,15 @@
 class PhotosController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     @place = Place.find(params[:place_id])
+    @place.photos.create(photo_params.merge(user: current_user))
     redirect_to place_path(@place)
   end
 
   private
 
-  def photos_params
+  def photo_params
     params.require(:photo).permit(:caption)
   end
 end
