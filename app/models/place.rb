4fe3c4b8f97633d2bd03fae2159aa4ffe975dcdf
rxso <rajat.soni@icloud.com>
2017-01-03 23:04:05 -0600

    Tweaks to the photo uploader feature from CarrierWave

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,8 @@ class Place < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   has_many :photos
+  has_many :captions
+
 
   geocoded_by :address
   after_validation :geocode
@@ -9,5 +11,4 @@ class Place < ActiveRecord::Base
   validates :name, presence: true, length: { minimum: 3 }
   validates :address, presence: true, length: { minimum: 10 }
   validates :description, presence: true, length: { minimum: 50 }
-  validates :caption, presence: true, length: { minimum: 8 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -28,31 +28,17 @@ ActiveRecord::Schema.define(version: 20170103223803) do
   add_index "comments", ["place_id"], name: "index_comments_on_place_id", using: :btree
   add_index "comments", ["user_id", "place_id"], name: "index_comments_on_user_id_and_place_id", using: :btree
 
-  create_table "models", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-  end
-
-  add_index "models", ["email"], name: "index_models_on_email", unique: true, using: :btree
-  add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true, using: :btree
-
   create_table "photos", force: true do |t|
     t.text     "caption"
-    t.text     "photo_ID"
+    t.integer  "user_id"
+    t.integer  "place_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "picture"
   end
+
+  add_index "photos", ["place_id"], name: "index_photos_on_place_id", using: :btree
+  add_index "photos", ["user_id", "place_id"], name: "index_photos_on_user_id_and_place_id", using: :btree
 
   create_table "places", force: true do |t|
     t.string   "name"
