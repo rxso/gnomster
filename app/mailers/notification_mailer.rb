4bfd35f56642db935198912ff0312f37b39c72e4
rxso <rajat.soni@icloud.com>
2017-01-09 11:12:06 -0600

    Fix syntax error in notificationmailer.rb

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,7 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@gnomsterapp.com"
 
-  def_comment_added
+  def comment_added
     mail(to: "rso544@gmail.com",
         subject: "A comment has been added to your place")
   end
