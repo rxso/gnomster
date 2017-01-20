04e765da9f2ea8b4bc8b5c296267f530743031a0
rxso <rajat.soni@icloud.com>
2017-01-20 12:07:05 -0600

    Add profile page functionality

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -1,7 +1,7 @@
 class AvatarsController < ApplicationController
   before_action :authenticate_user!
   def create
-    @user = User.find(params[:user_id])
+    @user = User.find(params[:id])
     @user.avatars.create(avatar_params)
     redirect_to root_path
   end
