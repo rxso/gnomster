fb9563d65916855f521eb5231538e64637732c02
rxso <rajat.soni@icloud.com>
2017-01-24 14:20:12 -0600

    Add most recent changes related to avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,18 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  before_action :authenticate_user!
+  def create
+    @user = User.find(params[:id])
+    @user.avatars.create(avatar_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def avatar_params
+    params.require(:user).permit(:email, :first_name, :last_name, :username, avatars: [])
+  end
+
+
 end
