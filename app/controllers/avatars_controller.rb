4a37c7c24efd7694809d7de4705f4be1642e416c
rxso <rajat.soni@icloud.com>
2017-01-30 09:18:59 -0600

    Changes to review section to fix formatting

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -1,16 +1,2 @@
 class AvatarsController < ApplicationController
-  before_action :authenticate_user!
-  def create
-    @user = User.find(params[:id])
-    @user.avatars.create(avatar_params)
-    redirect_to root_path
-  end
-
-  private
-
-  def avatar_params
-    params.require(:user).permit(:email, :first_name, :last_name, :username, avatars: [])
-  end
-
-
 end
